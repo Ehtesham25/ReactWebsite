dc7cc26e982ea6e366368d471a968753f7b2fe3b
EhteshamKhan <72070084+Ehtesham25@users.noreply.github.com>
2020-11-23 10:20:44 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/js/src/dropdown.js";
 import "../node_modules/bootstrap/js/src/collapse.js";
 // import "../src/Website/index.css";
-
 import "../src/Website/index.css";
 import App from "./Website/App";
 
