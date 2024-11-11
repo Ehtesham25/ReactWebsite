d2238aced07dcaeb277baa459ccbe2f622736d63
Ehtesham Khan <72070084+Ehtesham25@users.noreply.github.com>
2024-11-11 16:52:28 +0500

    Update App.jsx

--- src/Website/App.jsx
+++ src/Website/App.jsx
@@ -7,7 +7,7 @@ import Home from "../Website/Home";
 import NavBar from "../Website/NavBar";
 import Footer from "../Website/Footer";
  
-const App = ()=>{ 
+const App = ()=> { 
 return(
     <>
     <BrowserRouter>
