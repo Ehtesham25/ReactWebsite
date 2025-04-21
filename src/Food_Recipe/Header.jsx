61f6bae213c5ecff54fc14e64cef9327c9a43fe2
Ehtesham Khan <72070084+Ehtesham25@users.noreply.github.com>
2025-04-21 21:00:25 +0500

    Update Header.jsx

--- src/Food_Recipe/Header.jsx
+++ src/Food_Recipe/Header.jsx
@@ -6,7 +6,7 @@ const Header = (props)=>{
           <div className="jumbotron">
           <h1 className="display-1 main_head"> <i><FastfoodIcon/></i>Food Recipe </h1>
           <div className="input-group mb-3 w-50 mx-auto">
-  <input required type="text" className="form-control header_input" value={props.search} onChange={props.func} placeholder="Search your favorite Recipe..."  aria-describedby="basic-addon2"/>
+      <input required type="text" className="form-control header_input" value={props.search} onChange={props.func} placeholder="Search your favorite Recipe..."  aria-describedby="basic-addon2"/>
   <div className="input-group-append">
    <button onClick={props.Onclikcfunc} className="btn-dark btn-lg btn">SearchFood</button>
   </div>
