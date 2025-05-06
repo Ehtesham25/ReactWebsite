a7691bb1b7dbed799f2985f38c0419559d5bdfb1
Ehtesham Khan <72070084+Ehtesham25@users.noreply.github.com>
2025-05-06 19:01:59 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/js/src/dropdown.js";
 import "../node_modules/bootstrap/js/src/collapse.js";
 
-import "../src/Website/index.css";
+import "../src/Website/index.css"; 
 import App from "./Website/App";
 
 reactDOM.render(<App/>,document.getElementById("root"));
